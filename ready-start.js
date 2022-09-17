a3c5b5374d023aab4c3e7ce3817c5269c5ccf47f
qinhaitao97 <870554179@qq.com>
2022-09-17 10:46:32 +0800

    仅在 cookie 不存在的情况下创建 cookie

--- ready-start.js
+++ ready-start.js
@@ -1,5 +1,10 @@
+const fs = require('fs')
+const path = require('path')
 const clearCookie = require('./utils/clear-cookie')
 const makeSetting = require('./utils/make-setting')
+const { BASE_DIR, COOKIE_PATH } = require('./config')
 
-clearCookie()
+if (!fs.existsSync(path.join(BASE_DIR, COOKIE_PATH))) {
+  clearCookie()
+}
 makeSetting()
